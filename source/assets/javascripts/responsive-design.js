4cbe360ccbfca60dfec9653100007b48eee328df
Travis CI <developer@leeds.gov.uk>
2016-10-28 11:22:52 +0100

    fixed jquery issues

--- source/assets/javascripts/responsive-design.js
+++ source/assets/javascripts/responsive-design.js
@@ -1,4 +1,4 @@
-    (function (global) {
+  (function (global) {
     "use strict";
     var LCC = global.LCC || {};
 	    LCC.ResponsiveDesign = LCC.ResponsiveDesign || {};
@@ -6,6 +6,37 @@
     $(document).ready(function () {
         LCC.ResponsiveDesign.activate();
     });
+
+    //equal heights
+    LCC.ResponsiveDesign.equalheight = function(container){
+        var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0;
+
+        $(container).each(function() {
+            $el = $(this);
+            $($el).height('auto')
+            topPostion = $el.position().top;
+
+            if (currentRowStart != topPostion) {
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+                }
+                rowDivs.length = 0; // empty the array
+                currentRowStart = topPostion;
+                currentTallest = $el.height();
+                rowDivs.push($el);
+            } else {
+                rowDivs.push($el);
+                currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+            }
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+        });
+    }
 
     LCC.ResponsiveDesign.activate = function () {
         //main menu
@@ -24,44 +55,13 @@
             $('#nav-search input').focus();
         });
 
-        $(window).load(function() {
-             equalheight('.equal-item');
+        $(window).on('load', function() {
+            LCC.ResponsiveDesign.equalheight('.equal-item');
         });
 
-        $(window).resize(function(){
-             equalheight('.equal-item');
+        $(window).on('resize', function(){
+            LCC.ResponsiveDesign.equalheight('.equal-item');
         });
-
-        //equal heights
-        var equalheight = function(container){
-            var currentTallest = 0,
-                currentRowStart = 0,
-                rowDivs = new Array(),
-                $el,
-                topPosition = 0;
-
-            $(container).each(function() {
-                $el = $(this);
-                $($el).height('auto')
-                topPostion = $el.position().top;
-
-                if (currentRowStart != topPostion) {
-                    for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-                    rowDivs[currentDiv].height(currentTallest);
-                    }
-                    rowDivs.length = 0; // empty the array
-                    currentRowStart = topPostion;
-                    currentTallest = $el.height();
-                    rowDivs.push($el);
-                } else {
-                    rowDivs.push($el);
-                    currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-                }
-                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-                    rowDivs[currentDiv].height(currentTallest);
-                }
-            });
-        }
     }
    global.LCC = LCC;
 })(window)
