92362690fdaeeb1d7858ca499a9b96a6b5c001c9
Elliott <00772604@leeds.gov.uk>
2017-02-08 09:52:59 +0000

    Added google analytics

--- source/assets/javascripts/lcc-template.js
+++ source/assets/javascripts/lcc-template.js
@@ -6,6 +6,7 @@
 //= require accessible-media-player
 //= require sticky-ribbon
 //= require jquery-ui-datepicker
+//= require google-analytics
 //= require modules/back-to-top
 //= require modules/accordion
 //= require modules/carousel
