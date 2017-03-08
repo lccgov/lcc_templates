f6adb05375b45438bb56566b99166e26d9f1646f
Elliott <00772604@leeds.gov.uk>
2017-03-08 15:28:56 +0000

    Added datepicker.js

---
+++ source/assets/javascripts/datepicker.js
@@ -0,0 +1,45 @@
+(function (global, $) {
+    "use strict";
+    var LCC = global.LCC || {};
+	    LCC.Datepicker = LCC.Datepicker || {};
+
+    $(document).ready(function () {
+        $("input[data-type='datepicker-start']").datepicker({
+            defaultDate: null,
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            minDate: 0,
+            onSelect: function(selected) {
+                $("input[data-type='datepicker-end']").datepicker("option", "minDate", selected)
+            }
+        });
+        $("input[data-type='datepicker-end']").datepicker({
+            defaultDate: null,
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            minDate: 0,
+            onSelect: function (selected) {
+                $("input[data-type='datepicker-start']").datepicker("option", "maxDate", selected)
+            }
+        }); 
+        $("input[data-type='datepicker-start-nomin']").datepicker({
+            defaultDate: null,
+            dateFormat: "yy/mm/dd",
+            changeMonth: true,
+            onSelect: function(selected) {
+                $("input[data-type='datepicker-end-nomin']").datepicker("option", "minDate", selected)
+            }
+        });
+        $("input[data-type='datepicker-end-nomin']").datepicker({
+            defaultDate: null,
+            dateFormat: "yy/mm/dd",
+            changeMonth: true,
+            onSelect: function (selected) {
+                $("input[data-type='datepicker-start-nomin']").datepicker("option", "maxDate", selected)
+            }
+        });
+    });
+
+    global.LCC = LCC;
+
+})(window, jQuery);
